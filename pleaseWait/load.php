abcf4581cbb4f84720101b9b84c04e1b39ed13f4
Amzad Mojumder <amzad.FOF@gmail.com>
2014-04-23 17:51:42 +0100

    Please loader added

--- pleaseWait/load.php
+++ pleaseWait/load.php
@@ -4,10 +4,18 @@
  * User: AMojumder
  * Date: 23/04/14
  * Time: 17:21
- */ ?>
-
-Hcvc
-
-fgvcbcgfb
-
-dfsdsfjsdlkj jk sjdf jksdhk
+ */
+?>
+<style>
+    .back{
+        background : #<?=mt_rand(1,999999)?>;
+        color: #fff;
+    }
+</style>
+<div class="back">
+<?
+    echo(mt_rand() . "<br>");
+    echo(mt_rand() . "<br>");
+    echo(mt_rand(10,100));
+?>
+</div>
