3ace9918211b1ba2a7e6acdc7c5e0d5e8c28ad5e
Amzad Mojumder <amzad.FOF@gmail.com>
2014-04-24 17:42:05 +0100

    obj2xmlChild WIP

---
+++ xml/obj2xmlChild.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AMojumder
+ * Date: 23/04/14
+ * Time: 11:31
+ */
+
+/*
+ *  Covert from Obj to xml
+ *  Prints value with &gt; & &lt; 
+ */
+ini_set('display_errors', 1);
+ini_set('memory_limit', '512M');
+ini_set('error_log','var/log/errorPhp.log');
+
+class ObjectAndXML {
+//    private static $xml;
+
+    // Constructor
+    public function __construct() {
+//        $this->xml =  new SimpleXMLElement('<root/>');
+//        $this->xml->startDocument('1.0');
+    }
+
+    // Method to convert Object into XML string
+    public function objToXML($data, $xml, $newDoc=TRUE) {
+        if ($newDoc==TRUE){
+            $xml = new SimpleXMLElement("<root />");
+        }
+        $xml = $this->getArray2XML($data, $xml);
+        return ($newDoc==TRUE) ? $xml->asXML() : $xml ;
+    }
+
+    // Method to convert Object into XML string
+    public function getObject2XML($data, $xml) {
+        foreach($data as $key => $value){
+            echo '<br />This is a Key >> ',$key;
+            $keyChild =  (is_numeric($key))? $key :  'test';
+
+            if (is_array($value)){
+                $newXml = $xml->addChild('yesms');
+                $this->getArray2XML($value, $newXml);
+            }
+            else if(is_object($value)){
+                $newXml = $xml->addChild('yesms');
+                $this->getObject2XML($value, $newXml);
+            }
+            else
+                $xml->addChild( $key, $value);
+        }
+        return $xml;
+    }
+
+    // Method to convert Object into XML string
+    public function getArray2XML($data, $xml) {
+        foreach($data as $key => $value){
+            $keyChild =  (is_numeric($key))? $key :  'test';
+
+            if (is_array($value)){
+                $newXml = $xml->addChild('yesms');
+                $this->getArray2XML($value, $newXml);
+            }
+            else if(is_object($value)){
+                $newXml = $xml->addChild('yesms');
+                $this->getObject2XML($value, $newXml);
+            }else
+                $xml->addChild($key, $value);
+        }
+        return $xml;
+    }
+}
+
+
+
+$obj = new ObjectAndXML();
+$objData1 = new stdClass;
+
+
+$sweet1 = array('a' => 'apple', 'b' => 'banana');
+$sweet = array('a' => 'apple', 'b' => 'banana', 'newSweet' => $sweet1 );
+$fruits = array('sweet' => $sweet, 'sour' => 'lemon');
+$friend = array(  'age' => 23,  'name' => 'John',  'gender' => 'Male', 'eats' => $fruits );
+
+$objData1->records->person[0]->name = 'XYZ';
+$objData1->records->person[0]->age = '28';
+$objData1->records->person[0]->gender = 'Male';
+
+$objData1->records->person[1]->name = 'ABC';
+$objData1->records->person[1]->age =  '28';
+$objData1->records->person[1]->gender = 'Male';
+
+$objData1->records->person[1]->name = 'ABC';
+$objData1->records->person[1]->age =  '28';
+$objData1->records->person[1]->gender = 'Male';
+$objData1->records->person[1]->eats = $fruits;
+$objData1->records->person[1]->friend = $friend;
+
+
+$xml = "<root/>";
+
+echo $recordsXML = $obj->objToXML($objData1, $xml, TRUE);
+$dir = 'C:\Zend\Apache2\htdocs\play\xml\test\child.xml';
+file_put_contents($dir, $recordsXML);
+
+echo '<pre>';
+var_dump ($objData1);
