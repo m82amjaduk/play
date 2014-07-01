6569553d5ee58ce0613c8e62d3600525977fdc76
Amzad Mojumder <amzad.FOF@gmail.com>
2014-07-01 11:05:06 +0100

    WIP Store Locator!

--- ryman_stores/index.php
+++ ryman_stores/index.php
@@ -1,9 +1,16 @@
+<style>
+    .viewOnMap{
+        background-color: #000000;
+        color: #D2E0E6;
+    }
+</style>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
     <title>Ryman Store Locator</title>
     <script src="http://maps.google.com/maps/api/js?sensor=false"
             type="text/javascript"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script type="text/javascript">
         //<![CDATA[
 
@@ -90,9 +97,60 @@ try{
     echo 'Message: ' .$e->getMessage();
 }
 
-while($row = mysqli_fetch_array($result)) {
-    echo $row['branch_name']."<a href='store_details.php?store=".$row['id']."'> View Details </a>"."<hr>";
+while($row = mysqli_fetch_array($result)) { $store_id = $row['id'];
+    echo $row['branch_name']."<a href='store_details.php?store=".$store_id."'> View Details </a> ".
+        " <a href='#' store_id='$store_id' class='viewOnMap' > View On Map </a>"."<hr>";
+    //// Delete From here
+    echo $row['branch_name']."<a href='store_details.php?store=".$store_id."'> View Details </a> ".
+        " <a href='#' store_id='$store_id' class='viewOnMap' > View On Map </a>"."<hr>";
+    echo $row['branch_name']."<a href='store_details.php?store=".$store_id."'> View Details </a> ".
+        " <a href='#' store_id='$store_id' class='viewOnMap' > View On Map </a>"."<hr>";
+    echo $row['branch_name']."<a href='store_details.php?store=".$store_id."'> View Details </a> ".
+        " <a href='#' store_id='$store_id' class='viewOnMap' > View On Map </a>"."<hr>";
+    echo $row['branch_name']."<a href='store_details.php?store=".$store_id."'> View Details </a> ".
+        " <a href='#' store_id='$store_id' class='viewOnMap' > View On Map </a>"."<hr>";
+    echo $row['branch_name']."<a href='store_details.php?store=".$store_id."'> View Details </a> ".
+        " <a href='#' store_id='$store_id' class='viewOnMap' > View On Map </a>"."<hr>";
+    echo $row['branch_name']."<a href='store_details.php?store=".$store_id."'> View Details </a> ".
+        " <a href='#' store_id='$store_id' class='viewOnMap' > View On Map </a>"."<hr>";
+    echo $row['branch_name']."<a href='store_details.php?store=".$store_id."'> View Details </a> ".
+        " <a href='#' store_id='$store_id' class='viewOnMap' > View On Map </a>"."<hr>";
+    echo $row['branch_name']."<a href='store_details.php?store=".$store_id."'> View Details </a> ".
+        " <a href='#' store_id='$store_id' class='viewOnMap' > View On Map </a>"."<hr>";
+    echo $row['branch_name']."<a href='store_details.php?store=".$store_id."'> View Details </a> ".
+        " <a href='#' store_id='$store_id' class='viewOnMap' > View On Map </a>"."<hr>";
+    //Upto here
 }
 ?>
 </body>
 </html>
+
+
+<script>
+    $( ".viewOnMap" ).click(function() {
+        var store_id = $(this).attr("store_id");
+        alert( "Handler for .click() called." + store_id );
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        ///
+
+
+//        var map = new google.maps.Map(document.getElementById("map"), {
+//            center: new google.maps.LatLng(51.677, -0.606),
+//            zoom: 13,
+//            mapTypeId: 'roadmap'
+//        });
+//        var infoWindow = new google.maps.InfoWindow;
+//        var point = new google.maps.LatLng(
+//            parseFloat(51.677),
+//            parseFloat( -0.606);
+//        var html = "<b>" + name + "</b> <br/>"  + "<a href='store_details.php?store=" +"'> View Details </a>";
+////        var icon = customIcons[type] || {};
+//        var marker = new google.maps.Marker({
+//            map: map,
+//            position: point,
+//            icon: 'http://labs.google.com/ridefinder/images/mm_20_red.png',
+//            shadow: 'http://labs.google.com/ridefinder/images/mm_20_shadow.png'
+//        });
+//        bindInfoWindow(marker, map, infoWindow, html);
+    });
+</script>
