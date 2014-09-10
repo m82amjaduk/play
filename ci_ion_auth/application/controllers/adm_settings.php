cd1dbfbaa71cdbd6ec6ce6d77a7ec3b4531f2c2b
Amzad Mojumder <amzad.FOF@gmail.com>
2014-09-10 16:28:29 +0100

    WIP Update Working Fine

--- ci_ion_auth/application/controllers/adm_settings.php
+++ ci_ion_auth/application/controllers/adm_settings.php
@@ -6,14 +6,33 @@ class Adm_settings extends CI_Controller {
      * http://learning7.lc/ci_ion_auth/index.php/adm_settings
      */
     public function index() {
+        $this->data['name'] = 'test';
+        $this->load->view('adm/main.php', $this->data);
+    }
+    /*
+     * http://learning7.lc/ci_ion_auth/index.php/adm_settings
+     */
+    public function edit($table, $id) {
 
         $this->load->database();
-        $this->data['table'] = $this->db->field_data('settings_system');
-        $this->db->select('*') ->from('settings_system') ->where('id', 1);
+        $this->data['schema'] = $this->db->field_data($table);
+        $this->db->select('*') ->from($table) ->where('id', $id);
         $this->data['data'] = $this->db->get()->row() ;
+        $this->data['process'] = "adm_settings/edit_process/$table/$id";
+
 //        echo '<pre>'; print_r($this->data['data']); exit;
 
         $this->load->view('adm/main.php', $this->data);
+    }
+
+    public function edit_process($table, $id) {
+
+        $this->load->database();
+        $postData = $this->input->get(NULL, TRUE);
+        unset($postData['submitimage_x'],$postData['submitimage_y']);
+        echo '<pre>';print_r($postData);
+        $this->db->where('id', $id);
+        $this->db->update($table, $postData);
     }
 
 
--- ci_ion_auth/application/views/adm/main.php
+++ ci_ion_auth/application/views/adm/main.php
@@ -5,40 +5,61 @@
  * Date: 09/09/2014
  * Time: 15:10
  */
-//echo '<pre>'; print_r($table);
+//echo '<pre>'; print_r($schema);
+//echo '<pre>'; print_r($data);
+
+if(empty($data))
+    die('No Valid Data Found');
 ?>
 
-<form action="somescript.php" />
+<form action='http://learning7.lc/ci_ion_auth/index.php/<?=$process?>' />
 
-<? foreach ($table as $row){
+<?
+unset($schema[0], $schema[1], $schema[2]);
+echo "last Updated:: $data->updated <br /> ID:: $data->id";
+foreach ($schema as $row){
     $name = $row->name;
-    $input =array( 'varchar', 'int',  'smallint', 'timestamp' );
+    $input =array( 'varchar', 'smallint', 'int', 'timestamp' );
+
     if(in_array($row->type, $input) ){
-        echo getInput($row, $data->$name);
+        echo getInput($name, $data->$name);
     }
 
     if($row->type == 'tinyint' && ($row->max_length == 4 || $row->max_length == 1) ){
-        echo getEnable($row, $data->$name);
+        echo getEnable($name, $data->$name);
+    }else if($row->type == 'tinyint' && ($row->max_length != 4 || $row->max_length != 1) ){
+        echo getInput($name, $data->$name);
     }
     if($row->type == 'text' ){
-        echo getTextarea($row, $data->$name);
+        echo getTextarea($name, $data->$name);
     }
+    $allType = $input;
+    $allType[] = 'text';
+    $allType[] = 'tinyint';
+
+//echo '<pre>'; print_r($allType);
+
+    if(!in_array($row->type, $allType) ){
+        echo $row->type;
+        echo getInput($name, $data->$name);
+    }
+
 
 }?>
 <div><input type="image" name="submitimage" src="someimage.gif" /></div>
 </form>
 
 <?
-function getInput($row, $value){
-    return "<div>$row->name<input type='text' name='$row->name'' value='$value' /></div>";
+function getInput($name, $value){
+    return "<div>$name<input type='text' name='$name'' value='$value' /></div>";
 }
-function getEnable($row, $value){
+function getEnable($name, $value){
     $checkedYes = ($value == 1 )    ? "checked='checked'" : NULL;
     $checkedNo  = ($value != 1 )    ? "checked='checked'" : NULL;
-    return "<div> $row->name <input type='radio' name='$row->name' value='yes' $checkedYes /> yes <input type='radio' name='$row->name' value='no' $checkedNo /> no </div>";
+    return "<div> $name <input type='radio' name='$name' value='1' $checkedYes /> yes <input type='radio' name='$name' value='0' $checkedNo /> no </div>";
 }
-function getTextarea($row, $value){
-    return "<div>$row->name  <br /><textarea name='textarea' rows='10' cols='50'>$value</textarea></div>";
+function getTextarea($name, $value){
+    return "<div>$name <br /><textarea name='$name' rows='10' cols='50'>$value</textarea></div>";
 }
 ?>
 
