d2cb02a159d5330349fce2aea4ac66807d6f0aa8
Amzad Mojumder <amzad.FOF@gmail.com>
2014-09-18 14:14:43 +0100

    cms_nav_site_urls Added

--- nasreen/loyalty.sql
+++ nasreen/loyalty.sql
@@ -27,8 +27,10 @@ CREATE TABLE IF NOT EXISTS `loyalty_settings` (
   `purchase_earn` TINYINT(1) NOT NULL DEFAULT '0',
   `purchase_point` INT(11) NOT NULL DEFAULT '4' COMMENT 'For each pounds spent.',
 
-  `user_reg_earn` TINYINT(1) NOT NULL DEFAULT '0',
-  `user_reg_point` INT(11) NOT NULL DEFAULT '20',
+  `user_reg_guset_earn` TINYINT(1) NOT NULL DEFAULT '0',
+  `user_reg_guset_point` INT(11) NOT NULL DEFAULT '10',
+  `user_reg_reffered_earn` TINYINT(1) NOT NULL DEFAULT '0',
+  `user_reg_referred_point` INT(11) NOT NULL DEFAULT '50',
 
   `newsletter_earn` TINYINT(1) NOT NULL DEFAULT '0',
   `newsletter_point` INT(11) NOT NULL DEFAULT '10',
@@ -37,15 +39,20 @@ CREATE TABLE IF NOT EXISTS `loyalty_settings` (
   `blog_comment_point` INT(11) NOT NULL DEFAULT '50',
   `blog_comment_max_month` INT(11) NOT NULL DEFAULT '3' COMMENT 'Maximum Comment Post Each Month. For Comments, Point Will Not Be Earned If Limit Exit.',
 
-  `invitation_earn` TINYINT(1) NOT NULL DEFAULT '0',
-  `invitation_point` INT(11) NOT NULL DEFAULT '10',
+  `referral_earn` TINYINT(1) NOT NULL DEFAULT '0',
+  `referral_point` INT(11) NOT NULL DEFAULT '10',
 
-  `purchase_invitation_earn` TINYINT(1) NOT NULL DEFAULT '0',
-  `purchase_invitation_point` INT(11) NOT NULL DEFAULT '2' COMMENT 'For each pounds spent.',
-  `purchase_invitation_name_history` TINYINT(1) NOT NULL DEFAULT '1' COMMENT 'Show Friends Name On History',
+  `referral_purchase_first_earn` TINYINT(1) NOT NULL DEFAULT '0'  COMMENT 'Referer Get Points On FIRST order Only',
+  `referral_purchase_fisrt_point` INT(11) NOT NULL DEFAULT '5' COMMENT 'For each pounds spent.',
+  `referral_purchase_each_earn` TINYINT(1) NOT NULL DEFAULT '0' COMMENT 'Referer Get Points On EACH order',
+  `referral_purchase_each_point` INT(11) NOT NULL DEFAULT '2' COMMENT 'For each pounds spent.',
+  `referral_purchase_name_history` TINYINT(1) NOT NULL DEFAULT '1' COMMENT 'Show Friends Name On History',
+  
 
-  `point_worth_of` DECIMAL(7,2) NOT NULL DEFAULT '0.01' COMMENT 'Point Worth Of, In Cash Value',
-  `minimum_redeem` INT(11) NOT NULL DEFAULT '200' COMMENT 'Minimum Reward Points Balance to Be Able to Redeem',
+  `cash_order_earn` TINYINT(1) NOT NULL DEFAULT '0' COMMENT 'Earn Points On Cash Orders',
+  `auto_checked_use_reward` TINYINT(1) NOT NULL DEFAULT '0' COMMENT 'On Check Out Use Point Will Be Automatically Checked',
+  `point_worth_of` DECIMAL(7,2) NOT NULL DEFAULT '0.02' COMMENT 'Point Worth Of, In Cash Value',
+  `minimum_redeem` INT(11) NOT NULL DEFAULT '200' COMMENT 'Minimum Reward Points Balance to Be Able To Redeem',
   `point_expire_days` INT(11) NOT NULL DEFAULT '180' COMMENT 'Reward Points Expire in (days)',
 
   `email_point_update` TINYINT(1) NOT NULL DEFAULT '0' COMMENT 'Inform user about point update',
@@ -55,16 +62,15 @@ CREATE TABLE IF NOT EXISTS `loyalty_settings` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 COMMENT='###';
 
 
-
-DROP TABLE IF EXISTS `loyalty_invitation`;
-CREATE TABLE IF NOT EXISTS `loyalty_invitation` (
+DROP TABLE IF EXISTS `loyalty_referral`;
+CREATE TABLE IF NOT EXISTS `loyalty_referral` (
   `id` INT(11) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
   `login_id` BIGINT(20) NOT NULL DEFAULT '1',
   `updated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `active` TINYINT(1) NOT NULL DEFAULT '0',
 
-  `user_code_host` VARCHAR(7) NOT NULL DEFAULT 'AAA1786',
-  `user_code_guest` VARCHAR(7) DEFAULT NULL,
+  `user_code_referrer` VARCHAR(7) NOT NULL DEFAULT 'AAA1786',
+  `user_code_reffered` VARCHAR(7) DEFAULT NULL,
   `email` VARCHAR(1024) NOT NULL DEFAULT 'm82amjad@yahoo.co.uk',
   `email_valid` TINYINT(1) NOT NULL DEFAULT '0',
   `registered` TINYINT(1) NOT NULL DEFAULT '0' COMMENT 'Customer Registered',
@@ -122,3 +128,29 @@ INSERT INTO `cms_nav_url` (`id`, `login_id`, `updated`, `active`, `sn`, `parent`
   (00000000016, 1, '2014-09-16 14:50:17', 1, 5, 0, 'Sundries', 3, 'takeaway/menu/2', NULL),
   (00000000017, 1, '2014-09-16 14:50:17', 1, 1, 0, 'Meal Deals', 4, 'takeaway/menu/2', NULL);
 
+
+DROP TABLE IF EXISTS `cms_nav_site_urls`;
+CREATE TABLE IF NOT EXISTS `cms_nav_site_urls` (
+  `id` INT(11) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+  `login_id` BIGINT(20) NOT NULL DEFAULT '1',
+  `updated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `active` TINYINT(1) NOT NULL DEFAULT '0',
+
+  `href` VARCHAR(2048) NOT NULL DEFAULT 'new',
+  `title` VARCHAR(2048) NOT NULL DEFAULT 'Official Site | <?=$businessName?>',
+  `description` VARCHAR(2048) NOT NULL DEFAULT '<?=$description?>',
+  `keyword` VARCHAR(2048) NOT NULL DEFAULT '<?=$keyword?>, Chicken Tikka Masala, Lamb Dansak, Spicy, Hot,  Indian, Food, <?=$town?>',
+  `favicon` VARCHAR(2048) NOT NULL DEFAULT 'img/struct/favicon.ico',
+  `note` TEXT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 COMMENT='###';
+
+INSERT INTO `cms_nav_site_urls` (`id`, `login_id`, `updated`, `active`, `href`, `title`, `description`, `keyword`, `favicon`, `note`) VALUES
+  (00000000001, 1, '2014-09-18 11:35:38', 1, 'about/home', 'Official Site | Welcome To <?=$businessName?>', '<?=$description?>', '<?=$keyword?>, Chicken Tikka Masala, Lamb Dansak, Spicy, Hot,  Indian, Food, <?=$town?>', 'img/struct/favicon.ico', NULL),
+  (00000000002, 1, '2014-09-18 11:35:40', 1, 'takeaway/menu', '<?=$categoryName?> | Menu | <?=$businessName?>', '<?=$description?>', '<?=$keyword?>, Chicken Tikka Masala, Lamb Dansak, Spicy, Hot,  Indian, Food, <?=$town?>', 'img/struct/favicon.ico', NULL),
+  (00000000003, 1, '2014-09-18 11:35:41', 1, 'about/us', 'About Us | <?=$businessName?>', '<?=$description?>', '<?=$keyword?>, Chicken Tikka Masala, Lamb Dansak, Spicy, Hot,  Indian, Food, <?=$town?>', 'img/struct/favicon.ico', NULL),
+  (00000000004, 1, '2014-09-18 11:35:43', 1, 'about/gallery', 'Our Gallery | <?=$businessName?>', '<?=$description?>', '<?=$keyword?>, Chicken Tikka Masala, Lamb Dansak, Spicy, Hot,  Indian, Food, <?=$town?>', 'img/struct/favicon.ico', NULL),
+  (00000000005, 1, '2014-09-18 11:35:44', 1, 'about/contact', 'Contact Us | <?=$businessName?>', '<?=$description?>', '<?=$keyword?>, Chicken Tikka Masala, Lamb Dansak, Spicy, Hot,  Indian, Food, <?=$town?>', 'img/struct/favicon.ico', NULL),
+  (00000000006, 1, '2014-09-18 11:35:45', 1, 'about/specialoffers', 'Special Offers | <?=$businessName?>', '<?=$description?>', '<?=$keyword?>, Chicken Tikka Masala, Lamb Dansak, Spicy, Hot,  Indian, Food, <?=$town?>', 'img/struct/favicon.ico', NULL),
+  (00000000007, 1, '2014-09-18 11:35:53', 1, 'takeaway/menu/2', '<?=$categoryName?> | Menu | <?=$businessName?>', '<?=$description?>', '<?=$keyword?>, Chicken Tikka Masala, Lamb Dansak, Spicy, Hot,  Indian, Food, <?=$town?>', 'img/struct/favicon.ico', NULL),
+  (00000000008, 1, '2014-09-18 11:37:15', 1, 'takeaway/menu/3', '<?=$categoryName?> | Menu | <?=$businessName?>', '<?=$description?>', '<?=$keyword?>, Chicken Tikka Masala, Lamb Dansak, Spicy, Hot,  Indian, Food, <?=$town?>', 'img/struct/favicon.ico', NULL);
