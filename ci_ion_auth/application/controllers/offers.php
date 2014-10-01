fbff2ec27bffdb42bc895e4f84fe9aeb3e1d5a41
Amzad Mojumder <amzad.FOF@gmail.com>
2014-10-01 10:57:06 +0100

    WIP Offers

--- ci_ion_auth/application/controllers/offers.php
+++ ci_ion_auth/application/controllers/offers.php
@@ -231,3 +231,21 @@ COLLATE='latin1_swedish_ci'
 ENGINE=InnoDB
 AUTO_INCREMENT=0;*/
 
+/*
+INSERT INTO `offer_time_validity` (`id`, `login_id`, `updated`, `active`, `offer_id`, `day`, `from_`, `to_`) VALUES
+(00000000001, 1, '2014-10-01 09:53:22', 1, 1, 'Mon', '17:30', '23:59'),
+	(00000000002, 1, '2014-10-01 09:53:57', 1, 1, 'Tue', '17:30', '23:59'),
+	(00000000003, 1, '2014-10-01 09:54:07', 1, 1, 'Wed', '17:30', '23:59'),
+	(00000000004, 1, '2014-10-01 09:54:30', 1, 1, 'Thu', '17:30', '23:59'),
+	(00000000005, 1, '2014-10-01 09:54:40', 1, 1, 'Fri', '17:30', '23:59'),
+	(00000000006, 1, '2014-10-01 09:54:51', 1, 1, 'Sat', '17:30', '23:59'),
+	(00000000007, 1, '2014-10-01 09:55:02', 1, 1, 'Sun', '17:30', '23:59');
+
+INSERT INTO `offers` (`id`, `login_id`, `updated`, `active`, `sn`, `offer_code`, `title`, `summary`, `start_date`, `end_date`, `product_include`, `product_exclude`, `product_exclude_meal_deal`, `offers_page_show`, `offers_page_description`, `offers_page_terms`, `offers_page_readmore_link`, `email_template`, `admin_message`, `min_value`, `cw_discount_percentage`, `store_discount_percentage`, `cw_discount_amount`, `store_discount_amount`, `receiving`, `code_require`, `maximum_use`, `time_check`, `apply_loyalty_points`) VALUES
+	(00000000001, 1, '2014-10-01 09:41:50', 1, 1, 'ALLINEPOS', 'New', 'New', '2014-10-02 09:39:57', '2014-10-31 09:39:57', '#', '#', 0, 1, NULL, NULL, 'takeaway/menu/6', NULL, NULL, 10.00, 2.00, 2.00, 3.00, 1.00, 'collection_delivery', 1, -2, 1, 1);
+
+
+
+*/
+
+
