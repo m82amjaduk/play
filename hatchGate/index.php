8cd8f9b5dbc95cadad8c18498ebf266b28e1e5df
Amzad Mojumder <amzad.fof@gmail.com>
2014-11-21 09:51:09 +0000

    WIPPT

---
+++ hatchGate/index.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amzad
+ * Date: 20/11/14
+ * Time: 20:31
+ */ ?>
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
+    <title>Welcome to The Hatch Gate Inn</title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+
+    <meta name="viewport" content="width=device-width">
+
+    <!-- Google Web Font Embed -->
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,600,500,300,700' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<div id="main-wrapper">
+    <!--[if lt IE 7]>
+    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a rel="nofollow" href="http://browsehappy.com">upgrade your browser</a> or <a rel="nofollow" href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 affix text-center" style="z-index: 1;">
+        <h1 class="site-title">
+            <a href="#">The Hatch Gate Inn</a>
+            <img src="images/btn-menu.png" alt="main menu" id="m-btn" class="pull-right visible-xs visible-sm" >
+        </h1>
+
+        <ul id="responsive" style="display:none" class="hidden-lg hidden-md"></ul><!-- /.responsive -->
+    </div>
+
+    <div class="menu visible-md visible-lg">
+        <ul id="menu-list">
+            <li class="active home-menu"><a href="#home">Home</a></li>
+            <li class="about-menu"><a href="#about">About Us</a></li>
+            <li class="services-menu"><a href="#services">Our Services</a></li>
+            <li class="testimonial-menu"><a href="#testimonial">Testimonials</a></li>
+            <li class="contact-menu"><a href="#contact">Contact</a></li>
+        </ul>
+    </div><!-- /.menu -->
+
+    <div class="image-section">
+        <div class="image-container">
+            <img src="images/Hatch-Gate-Inn-01.jpg" id="home-img" class="main-img inactive" alt="Home">
+            <img src="images/Hatch-Gate-Inn-02.jpg" id="about-img" class="inactive" alt="About">
+            <img src="images/Hatch-Gate-Inn-03.jpg" id="services-img"  class="inactive" alt="Services">
+            <img src="images/Hatch-Gate-Inn-04.jpg" id="testimonial-img" class="inactive" alt="Testimonial">
+            <img src="images/Hatch-Gate-Inn-05.jpg" id="contact-img" class="inactive" alt="Contact">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 col-lg-offset-6 col-md-offset-6 content-wrapper">
+            <div class="content">
+
+                <section id="home-text" class="active content-section">
+                    <div class="col-sm-12 col-md-12 col-lg-12">
+                        <h2>The atmosphere of <span class="coloured">luxury.</span> The spirit of <span class="coloured">delight.</span></h2>
+                        <p>The Hatchgate Inn is a XV1th century Inn with lots of character, oak beams and a large open inglenook fireplace with original artefacts. It is a pub that welcomes everybody, with a large garden, ample parking and an enclosed play area for younger children. For sports fans we have Sky TV in the Sports Bar (we are a member of the London Irish Supporters Club).</p>
+                        <h4><i>Welcome to The Hatch Gate Inn .. </i></h4>
+                    </div>
+                </section><!-- /.home-text -->
+
+                <section id="about-text" class="inactive">
+                    <div class="col-sm-12 col-md-12 col-lg-12">
+                        <h2>About Us</h2>
+                        <p>The Hatchgate Inn is a XV1th century Inn with lots of character, oak beams and a large open inglenook fireplace with original artefacts. It is a pub that welcomes everybody, with a large garden, ample parking and an enclosed play area for younger children. For sports fans we have Sky TV in the Sports Bar (we are a member of the London Irish Supporters Club).</p>
+                        <h4>Welcome to The Hatch Gate Inn .. </h4>
+                    </div>
+                </section><!-- /.about-text -->
+
+                <section id="services-text" class="inactive">
+                    <div class="col-sm-12 col-md-12 col-lg-12">
+                        <h2>About Us</h2>
+                        <p>The Hatchgate Inn is a XV1th century Inn with lots of character, oak beams and a large open inglenook fireplace with original artefacts. It is a pub that welcomes everybody, with a large garden, ample parking and an enclosed play area for younger children. For sports fans we have Sky TV in the Sports Bar (we are a member of the London Irish Supporters Club).</p>
+                        <h4>Welcome to The Hatch Gate Inn .. </h4>
+                    </div>
+                </section><!-- /.services-text -->
+
+                <section id="testimonial-text" class="inactive">
+                    <div class="col-sm-12 col-md-12 col-lg-12">
+                        <h2>About Us</h2>
+                        <p>The Hatchgate Inn is a XV1th century Inn with lots of character, oak beams and a large open inglenook fireplace with original artefacts. It is a pub that welcomes everybody, with a large garden, ample parking and an enclosed play area for younger children. For sports fans we have Sky TV in the Sports Bar (we are a member of the London Irish Supporters Club).</p>
+                        <h4>Welcome to The Hatch Gate Inn .. </h4>
+                    </div>
+                </section><!-- /.testimonial-text -->
+
+                <section id="contact-text" class="inactive">
+                    <div class="col-sm-12 col-md-12">
+                        <div class="row">
+                            <div class="col-sm-12 col-md-12"><h2>Contact</h2></div>
+                            <div class="clearfix"></div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-6 col-md-6">
+                                <div id="map-canvas"></div>
+                                <p>The Hatch Gate Inn</p>
+                                <p>Burghfield Village, Burghfield</p>
+                                    <p>RG30 3TH</p>
+                            </div>
+
+                            <div class="col-sm-6 col-md-6">
+                                <form action="#" method="post">
+
+                                    <div class="form-group">
+                                        <!--<label for="contact_name">Name:</label>-->
+                                        <input type="text" id="contact_name" class="form-control" placeholder="Name" />
+                                    </div>
+                                    <div class="form-group">
+                                        <!--<label for="contact_email">Email:</label>-->
+                                        <input type="text" id="contact_email" class="form-control" placeholder="Email Address" />
+                                    </div>
+                                    <div class="form-group">
+                                        <!--<label for="contact_message">Message:</label>-->
+                                        <textarea id="contact_message" class="form-control" rows="9" placeholder="Write a message"></textarea>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Send</button>
+
+                                </form>
+                            </div>
+
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </section><!-- /.contact-text -->
+            </div><!-- /.content -->
+        </div><!-- /.content-wrapper -->
+    </div><!-- /.row -->
+
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 footer">
+            <p class="footer-text">Copyright &copy; 2014 conosurTek Ltd  - Design: <a href="http://www.allinepos.co.uk">All In ePOS</a></p>
+        </div><!-- /.footer -->
+    </div>
+
+</div><!-- /#main-wrapper -->
+
+<div id="preloader">
+    <div id="status">&nbsp;</div>
+</div><!-- /#preloader -->
+
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery.backstretch.min.js"></script>
+<script src="js/script.js"></script>
+<!-- templatemo 398 nature -->
+</body>
+</html>
