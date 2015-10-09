23a7a77bf6f00319cd554f3e85866ad724c320bc
Amzad Mojumder <amzad.FOF@gmail.com>
2015-10-09 10:07:49 +0100

    php add line to file

---
+++ file/addline.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AMojumder
+ * Date: 09/10/2015
+ * Time: 09:35
+ */
+
+$file = 'addline.csv';
+$header = "Name, IP,  host, RAM, Task, NS \n";
+$data = file_get_contents($file);
+file_put_contents($file, $header.$data);
+
+
+$data = file_get_contents($file);
+print_r($data);
